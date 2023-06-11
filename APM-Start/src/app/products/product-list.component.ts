65f65dae65c59076aae90f517da8725bfd7e8ac3
arelymartinez16 <arelymartinez741@gmail.com>
2023-06-11 14:02:13 -0400

    Built ProductList Component

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+// this makes the class a component
+@Component({
+    selector: "pm-products",
+    templateUrl: "./product-list-component.html"
+})
+
+export class ProductListComponent {
+
+}
