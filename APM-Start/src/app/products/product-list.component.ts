32f230439985aaa6edb2cb808940f29e6c47fa27
arelymartinez16 <arelymartinez741@gmail.com>
2023-07-06 17:04:18 -0400

    Input gets filtered, and the specific products show as the user types

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -22,7 +22,9 @@ export class ProductListComponent implements OnInit {
     set listFilter(value: string) {
         this._listFilter = value;
         console.log("In setter: ", value);
+        this.filteredProducts = this.performFilter(value)
     }
+    filteredProducts: IProduct[] = [];
     products: IProduct[] = [
         {
             "productId": 1,
@@ -53,5 +55,10 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         this.listFilter = "cart";
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy))
     }
 }
