09d11fadfe892667f7fd4fe7e23adfa5aa18d9d5
arelymartinez16 <arelymartinez741@gmail.com>
2023-07-08 18:13:21 -0400

    Handling null and undefined

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,12 +8,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
+  product: IProduct | undefined;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get("id"));
     this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": id,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2020",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "assets/images/xbox-controller.png"
+    }
   }
 
 }
