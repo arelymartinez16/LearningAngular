9fd35c7763ef26c5d7978efeb17fe7f4f5b379ce
arelymartinez16 <arelymartinez741@gmail.com>
2023-07-08 12:28:24 -0400

    Injected the service to the ProductListComponent

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 // this makes the class a component
 @Component({
@@ -25,36 +26,22 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.performFilter(value)
     }
     filteredProducts: IProduct[] = [];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        } 
-    ];
+    products: IProduct[] = [];
+
+    constructor(private productService: ProductService) {
+
+    }
 
     // methods are placed below the properties
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
+    // component initialization
     ngOnInit(): void {
-        this.listFilter = "cart";
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
+        // this.listFilter = "cart";
     }
 
     performFilter(filterBy: string): IProduct[] {
